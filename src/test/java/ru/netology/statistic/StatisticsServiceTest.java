e4dda30d71b389060ec754cd986bbc91821e13bb
Prishutova <belyaevazina94@gmail.com>
2025-08-14 10:22:36 +0300

    Update StatisticsServiceTest.java

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -24,16 +24,21 @@ public class StatisticsServiceTest {
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 13, 8, 6, 11, 11, 12};
         long expected = 13;
+        
         long actual = service.findMax(incomesInBillions);
+        
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
     void findMaxWithNegativeNumber() {
         StatisticsService service = new StatisticsService();
+        
         long[] incomesInBillions = {12, -13, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+        
         long actual = service.findMax(incomesInBillions);
+        
         Assertions.assertEquals(expected, actual);
     }
 }
